1f2862e0d4bb756bf077ae0431d30e954a8b6ffc
Rafael Sampaio Tavares <me@rafaelst.dev>
2023-08-17 20:24:44 -0300

    feat: navbar menu mobile

--- src/presentation/components/NavBar/NavBar.tsx
+++ src/presentation/components/NavBar/NavBar.tsx
@@ -1,25 +1,45 @@
-import { FC } from 'react'
+import { FC, useMemo, useState } from 'react'
+import { createPortal } from 'react-dom'
+import classNames from 'classnames/bind'
 
 import logo from '@/presentation/assets/logo.png'
-import iconWorld from '@/presentation/assets/world.svg'
+import { ReactComponent as IconWorld } from '@/presentation/assets/icons/world.svg'
+import iconMenu from '@/presentation/assets/icons/menu.svg'
 import styles from './NavBar.module.scss'
 
+const cx = classNames.bind(styles)
+
 export const NavBar: FC = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const renderMenu = useMemo(() => (
+    <ul className={styles.menu}>
+      <li className={styles.menuItem}>
+        <a href="https://www.vittude.com/" className={styles.link}>Site institucional</a>
+      </li>
+      <li className={styles.menuItem}>
+        <a href="https://www.vittude.com/blog/" className={styles.link}>Blog</a>
+      </li>
+      <li className={styles.menuItem}>
+        <a href="" className={styles.link} title="Selecionar linguagem"><IconWorld /></a>
+      </li>
+    </ul>
+  ), [])
+
   return (
     <div className={styles.container}>
       <img className={styles.logo} src={logo} alt="Logo da Vittude" />
 
-      <ul className={styles.menu}>
-        <li>
-          <a href="" className={styles.menuItem}>Site institucional</a>
-        </li>
-        <li>
-          <a href="" className={styles.menuItem}>Blog</a>
-        </li>
-        <li>
-          <a href="" className={styles.menuItem} title="Selecionar linguagem"><img src={iconWorld} alt="Linguagem" /></a>
-        </li>
-      </ul>
+      <div className={styles.menuDesktop}>
+        {renderMenu}
+      </div>
+      <div className={styles.menuMobile}>
+        <img className={styles.menuMobileIcon} src={iconMenu} onClick={() => { setShowMenu(true) }} />
+        <div className={cx(styles.bottomSheet, showMenu && styles.bottomSheetOpen)}>
+          {showMenu && createPortal(<div className={styles.backdrop} onClick={() => { setShowMenu(false) }} />, document.body)}
+          {renderMenu}
+        </div>
+      </div>
     </div>
   )
 }
