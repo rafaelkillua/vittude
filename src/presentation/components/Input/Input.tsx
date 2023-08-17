33164c3e4c0088120641d0edadfb39e586c93f27
Rafael Sampaio Tavares <me@rafaelst.dev>
2023-08-17 19:23:55 -0300

    feat: show and hide password

--- src/presentation/components/Input/Input.tsx
+++ src/presentation/components/Input/Input.tsx
@@ -1,21 +1,45 @@
-import { forwardRef, useId } from "react"
+import { forwardRef, useId, useMemo, useState } from "react"
 import classNames from "classnames/bind"
 
 import { IInputProps } from "@/domain"
 import styles from './Input.module.scss'
 
+import eye from '@/presentation/assets/eye.svg'
+import eyeHide from '@/presentation/assets/eye-hide.svg'
+
 const cx = classNames.bind(styles)
 
 export const Input = forwardRef<HTMLInputElement, IInputProps>(function Input(props, ref) {
-  const { id, className, label, ...rest } = props
+  const { id, className, label, type, ...rest } = props
 
   const uid = useId()
   const inputId = id ?? uid
 
+  const [showPassword, setShowPassword] = useState(false)
+
+  const toggleShowPassword = () => {
+    setShowPassword(oldValue => !oldValue)
+  }
+
+  const inputType = useMemo(() => {
+    if (type === 'password' && showPassword) {
+      return 'text'
+    }
+    return type
+  }, [type, showPassword])
+
   return (
     <div className={cx(styles.inputContainer, className)}>
       <label className={styles.label} htmlFor={inputId}>{label}</label>
-      <input className={styles.input} id={inputId} ref={ref} {...rest} />
+      <input className={styles.input} id={inputId} ref={ref} type={inputType} {...rest} />
+      {type === 'password' && (
+        <img
+          className={styles.showPasswordIcon}
+          src={showPassword ? eyeHide : eye}
+          onClick={toggleShowPassword}
+          aria-hidden
+        />
+      )}
     </div>
   )
 })
